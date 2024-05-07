04b8bfd90010f8f2a84aba8f9f9437743dadcb03
Farid Andika <faridandika159@gmail.com>
2024-05-07 08:58:08 +0700

    Initial Commit

---
+++ First UIKIT APP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  First UIKIT APP
+//
+//  Created by Farid Andika on 07/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
